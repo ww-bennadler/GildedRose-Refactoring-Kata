8c04b457bcd8e674c704cea564a44937fcf9afbe
ben.nadler <ben.nadler@wnwd.com>
2024-08-20 10:12:02 +0300

    aug 20 refactoring - from yesterday

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,11 @@
+enum ItemName {
+  AgedBrie = 'Aged Brie',
+  BackstagePasses = 'Backstage passes to a TAFKAL80ETC concert',
+  Sulfuras = 'Sulfuras, Hand of Ragnaros'
+}
+
+const knownItemNames = [ItemName.AgedBrie, ItemName.BackstagePasses, ItemName.Sulfuras];
+
 export class Item {
   name: string;
   sellIn: number;
@@ -17,49 +25,36 @@ export class GildedRose {
     this.items = items;
   }
 
+  calculateQuality({ item, notInKnownNamesAndPositiveQuality }) {
+    let { name, quality, sellIn } = item ?? {};
+    if (notInKnownNamesAndPositiveQuality) {
+      quality = quality - 1
+    } else if (quality < 50) {
+      quality = quality + 1
+      if (name == ItemName.BackstagePasses && sellIn < 11) {
+        quality = quality + 1
+      }
+
+      if (name == ItemName.BackstagePasses && sellIn < 6) {
+        quality = quality + 1
+      }
+    }
+
+    return quality;
+  }
+
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+    for (let item of this.items) {
+      let { name, quality, sellIn } = item ?? {};
+      const notInKnownNamesAndPositiveQuality = !knownItemNames.includes(name as ItemName) && quality > 0;
+      sellIn = name != ItemName.Sulfuras ? sellIn - 1 : sellIn;
+      quality = this.calculateQuality({ item, notInKnownNamesAndPositiveQuality });
+
+      if (sellIn < 0) {
+        if (name != ItemName.AgedBrie) {
+          quality = notInKnownNamesAndPositiveQuality ? quality - 1 : 0;
+        } else if (quality < 50) {
+            quality = quality + 1
         }
       }
     }
